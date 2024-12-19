c131c5dbff1d8c22f0a26d85285a89ecfe317352
TheMoonBeard <themoonbeard@gmail.com>
2024-12-19 10:54:44 +0100

    Test Commit

--- Assets/Timotee/Scripts/SasController.cs
+++ Assets/Timotee/Scripts/SasController.cs
@@ -1,8 +1,12 @@
+using System;
 using UnityEngine;
 
 public class SasController : MonoBehaviour
 {
     private bool _closed = true;
-    
-    
+
+    private void Update()
+    {
+        Debug.Log("Lol");
+    }
 }
